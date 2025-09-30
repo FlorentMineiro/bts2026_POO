215e4591667d85e17264ca15f2ad7ad57b5cf570
Florent Mineiro <florent.mineiro.93@gmail.com>
2025-09-30 09:27:43 +0200

    Ajout de la Classe Dog

---
+++ Dog.php
@@ -0,0 +1,10 @@
+<?php
+
+ class Dog extends Animal{
+
+    public function __construct(String $name)
+    {
+      parent::__construct($name);
+    } 
+    
+ }
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 //inclure la classe Cat
 include "Animal.php";
 include "Cat.php";
+include "Dog.php";
 
 
 //instancier l'objet de la classe cat
@@ -10,4 +11,7 @@ include "Cat.php";
 $monPremierChat = new Cat("Garfield");
 $monPremierChat -> setAge(30);
 
-echo "Mon premier chat, il s'appelle".$monPremierChat -> getName()."et son age :".$monPremierChat->getAge();
+$monPremierChien = new Dog("Medore");
+
+echo "Mon premier chat, il s'appelle : ".$monPremierChat -> getName()." et son age :".$monPremierChat->getAge()."\n";
+echo "Mon premier chien s'appelle :".$monPremierChien -> getName();
